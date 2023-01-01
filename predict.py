889ceee1e26096884b362fcf3f6f0359a5570b0a
VincentLi1216 <sunnus.tw@gmail.com>
2023-01-01 12:14:42 +0800

    all the basic functions are done

--- predict.py
+++ predict.py
@@ -6,61 +6,123 @@ import numpy as np
 from matplotlib import pyplot as plt
 import mediapipe as mp
 import time
-from util_progress_bar import progress_bar
+from util_warning import *
 
 # init the mp_face_detection
 mp_face_detection = mp.solutions.face_detection
 mp_drawing = mp.solutions.drawing_utils
 
+#load the gaydar model
 model = load_model('models/imageclassifier2.h5')
 
-def prediction(dir):
-    img = cv2.imread(dir)
-    # cv2.imshow("img", img)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+#predict how gay are you
+def prediction(img):
+    import cv2
+
+    #if there's nothing pass in then return
+    if img is None:
+        return
+
+    #convert to the correct color format
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
+    #resize the image again just for sure
     resize = tf.image.resize(img, (256, 256))
+
+    #predict the model
     yhat = model.predict(np.expand_dims(resize / 255, 0))
-    print(yhat)
-    if yhat > 0.5:
-        print(f'Predicted class is Straight')
-    else:
-        print(f'Predicted class is Gay')
+
+    #print out the result
+    print(round(100-yhat[0,0]*100, 2) ,"% is Gay")
+
+    #show the result
+    # todo: change the title of the window
     plt.imshow(img)
     plt.show()
 
 
 # use mp_face_detection to find the faces in the image than crop it to size 100*100 pixels in the format of .jpg
-def crop_face(folder_name, to_show = False):
+def crop_face(img_path, to_show = False):
     with mp_face_detection.FaceDetection(model_selection=1, min_detection_confidence=0.9) as face_detection:
-        time.sleep(1)  # in order to get the progress bar display normally
 
         # print out all the file it gets
         print("Working process:")
-        for idx, path in enumerate(os.listdir(folder_name)):
-            if path.endswith(".DS_Store"):
-                continue
-            image = cv2.imread(os.path.join(folder_name, path))  # read the img
-            progress_bar(idx, len(os.listdir(folder_name)))  # update the progress bar
-            orig_shape = [image.shape[1], image.shape[0]]  # get the shape of the img
-            results = face_detection.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))  # convert the BGR image to RGB and process it with MediaPipe Face Detection
-            orig_image = image.copy()  # get the orig copy
 
-            # no faces detected
-            if not results.detections:
-                continue
+        image = cv2.imread(img_path)  # read the img
+        # progress_bar(idx, len(os.listdir(folder_name)))  # update the progress bar
+        orig_shape = [image.shape[1], image.shape[0]]  # get the shape of the img
+        results = face_detection.process(cv2.cvtColor(image,
+                                                      cv2.COLOR_BGR2RGB))  # convert the BGR image to RGB and process it with MediaPipe Face Detection
+        orig_image = image.copy()  # get the orig copy
 
-            # face detected
-            if results.detections:
-                for detection in results.detections:
-                    mp_drawing.draw_detection(image, detection)
-                    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-                    plt.show()
-                    # crop_img(orig_image, detection, orig_shape, path=folder_name + "/cropped", idx=idx, to_show=to_show)  # crop the img
+        # no faces detected
+        if not results.detections:
+            # print(os.path.join(img_path, path) + " has no faces detected!")
+            warning("NO Faces Detected!")
+            return None
+
+        # face detected
+        if results.detections:
+            for detection in results.detections:
+                mp_drawing.draw_detection(image, detection)
+                cv2.imshow("Face Detected", image)
+                cv2.waitKey(0)
+                cv2.destroyWindow("Face Detected")
+                return crop_img(orig_image, detection, orig_shape, to_show=to_show)  # crop the img
+
+#to crop img
+def crop_img(img, detection, orig_shape, to_show = False):
+    # get all the info to crop
+    orig_width = orig_shape[0]
+    orig_height = orig_shape[1]
+
+    xmin = int(detection.location_data.relative_bounding_box.xmin * orig_width)
+    ymin = int(detection.location_data.relative_bounding_box.ymin * orig_height)
+    width = int(detection.location_data.relative_bounding_box.width * orig_width)
+    height = int(detection.location_data.relative_bounding_box.height * orig_height)
+
+    img = img[ymin:ymin+height, xmin:xmin+width]  # got to reverse the order of x and y
+
+    # show the img if necessary
+    if to_show == True:
+        cv2.imshow("cropped", img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
 
+    try:
+        crp_img = cv2.resize(img, (256, 256))
+        # cv2.imwrite(path+"/"+IMAGE_NAMES[idx],img)
+        return crp_img
+    except:
+        # print out failed img's filename
+        print()
+        print("crop error")
+        print("can Not crop the image")
+
+#the main func
+def main(folder_name = "test1/orig"):
+
+    #get all the file in the folder
+    for idx, path in enumerate(os.listdir(folder_name)):
+
+        #only read the .jpg files
+        if not path.endswith(".jpg"):
+            continue
+
+        #print out img's name
+        print(path)
+
+        #show the orig img
+        image = cv2.imread(os.path.join(folder_name, path))
+        cv2.imshow(path, image)
+        cv2.waitKey(0)
+        cv2.destroyWindow(path)
+
+        #firstly crop the img then predict it
+        prediction(crop_face(os.path.join(folder_name, path)))
 
 
-crop_face("test1/rename")
+if __name__ == "__main__":
+    main("test2/orig")
+
---
+++ util_warning.py
@@ -0,0 +1,12 @@
+def warning(warning_mes):
+    print()
+    print()
+    print()
+    print("-----------------WARNING-----------------")
+    print()
+    print(warning_mes)
+    print()
+    print("-------------------END-------------------")
+    print()
+    print()
+    print()
